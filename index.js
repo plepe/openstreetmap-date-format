01e9fd3edf0eeb6186a4f2a8082f5f639b2f2cf8
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2019-01-12 21:35:41 +0100

    Format date localized

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const moment = require('moment')
 const templates = {
   single: '%1',
   before: 'vor %1',
-  range: 'zwischen %1 und %2'
+  range: 'zwischen %1 und %2',
+  formatDay: 'LL',
+  formatMonth: 'MMMM YYYY',
+  formatYear: 'YYYY',
+  formatCentury: '%d. Jahrhundert',
+  formatDecade: '%der Jahre'
 }
 
 function formatDate(date) {
-  return moment(date).format()
+  let format
+  let m = date.match(/^([0-9]{4})(\-[0-9]{2})?(\-[0-9]{2})?$/)
+  if (m) {
+    if (m[3]) {
+      format = templates.formatDay
+    } else if (m[2]) {
+      format = templates.formatMonth
+    } else {
+      format = templates.formatYear
+    }
+
+    return moment(date).format(format)
+  }
+
+  m = date.match(/^C([0-9]+)$/i)
+  if (m) {
+    return templates.formatCentury.replace('%d', parseInt(m[1]))
+  }
+
+  m = date.match(/^([0-9]{4})s$/i)
+  if (m) {
+    return templates.formatDecade.replace('%d', m[1])
+  }
 }
 
 function parse (date) {
@@ -38,5 +65,9 @@ function parse (date) {
 
 console.log(parse('1954'))
 console.log(parse('before 1954'))
+console.log(parse('before 1954-04'))
 console.log(parse('1954..1966'))
 console.log(parse('1954-08-13..1966-01-01'))
+console.log(parse('C16'))
+console.log(parse('before C16'))
+console.log(parse('C19..1920s'))
