185c075c24fb178a15f820171a719d01158d0b40
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2019-10-04 14:28:06 +0200

    CLI: use locale from env

--- cli.js
+++ cli.js
@@ -6,6 +6,15 @@ const rl = readline.createInterface({
   input: process.stdin
 })
 
+// detect locale
+let [ , lang ] = process.env.LANG.match(/^([a-z]+)[_\.]/)
+const locales = require('./locale/list.json')
+if (!locales.includes(lang)) {
+  lang = locales[0]
+}
+
+osmDateFormat.locale(lang)
+
 rl.on('line', input => {
   console.log(osmDateFormat(input))
 })
