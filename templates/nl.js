214782234caa7b0f71954388fd6a4b9ae1e0c215
danieldegroot2 <67521919+danieldegroot2@users.noreply.github.com>
2022-12-08 23:31:13 +0100

    Create Dutch translation
    Created Dutch translation

---
+++ templates/nl.js
@@ -0,0 +1,71 @@
+const moment = require('moment')
+const ordinal = require('ordinal')
+
+function formatDate (options, date) {
+  let result = date.value
+
+  switch (date.type) {
+    case 'day':
+      if (options.format === 'short') {
+        result = moment(date.value).format('ll')
+      } else {
+        result = moment(date.value).format('LL')
+      }
+      break
+    case 'month':
+      if (options.format === 'short') {
+        result = moment(date.value).format('MMM YYYY')
+      } else {
+        result = moment(date.value).format('MMMM YYYY')
+      }
+      break
+    case 'year':
+      result = date.value
+      break
+    case 'decade':
+      result = 'de jaren ' + date.value
+      break
+    case 'century':
+      if (parseInt(date.value) === 1) {
+        result = date.value += 'e eeuw'
+      } else {
+        result = date.value += 'ste eeuw'
+      }
+  }
+
+  switch (date.prefix) {
+    case 'before':
+      result = 'voor ' + result
+      break
+    case 'after':
+      result = 'na ' + result
+      break
+    case 'early':
+      result = 'begin ' + result
+      break
+    case 'mid':
+      result = 'midden ' + result
+      break
+    case 'late':
+      result = 'eind ' + result
+      break
+  }
+
+  if (date.circa) {
+    result = 'ca. ' + result
+  }
+
+  if (date.bc) {
+    result += ' v. Chr.'
+  }
+
+  return result
+}
+module.exports = {
+  single: function (options, value) {
+    return formatDate(options, value, false)
+  },
+  range: function (options, value1, value2) {
+    return 'tussen ' + formatDate(options, value1, true) + ' en ' + formatDate(options, value2, true)
+  }
+}
